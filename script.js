4da65dda16884afcc154df2a12182f651f15c232
Hamim Hossain <Hamim404@proton.me>
2026-06-30 17:15:27 +0600

    Activity Function Added

--- script.js
+++ script.js
@@ -22,16 +22,44 @@ function setInnerText(id, value) {
 // Call Alert Message
 function callFunction(id, message) {
   document.getElementById(id).addEventListener("click", function () {
-    if(coinAmount <20){
-        alert("❌ You don't have enough coins! You need at least 20 coins to make a call.")
-        return;
+    if (coinAmount < 20) {
+      alert(
+        "❌ You don't have enough coins! You need at least 20 coins to make a call.",
+      );
+      return;
     }
     alert(message);
     coinAmount = coinAmount - 20;
-    setInnerText("coins", coinAmount)
+    setInnerText("coins", coinAmount);
   });
 }
 
+//History Area Function
+function makeNewElement(id, title, number) {
+  document.getElementById(id).addEventListener("click", function () {
+    if (coinAmount < 20) {
+      return;
+    }
+    const element = document.createElement("div");
+    const date = new Date().toLocaleTimeString();
+    const historyArea = document.getElementById("history-area");
+
+    element.innerHTML = `
+    <div
+            class="flex justify-between items-center bg-gray-200 p-2 rounded-lg"
+          >
+            <div>
+              <h4 class="font-bold">${title}</h4>
+              <p class="text-gray-500">${number}</p>
+            </div>
+            <div>
+              <p class="text-gray-600">${date}</p>
+            </div>
+          </div>
+    `;
+    historyArea.appendChild(element);
+  });
+}
 // Heart Count Function
 const hearts = document.getElementsByClassName("fa-heart");
 let heartNumber = getInnerTextNumber("heart-count");
@@ -43,18 +71,21 @@ for (const heart of hearts) {
   });
 }
 
+// History Function
+makeNewElement("national-call", "National Emergency", "999");
+makeNewElement("police-call", "Police Helpline", "999");
+makeNewElement("fire-call", "Fire Service", "999");
+makeNewElement("ambulance-call", "Ambulance Service", "1994-999999");
+makeNewElement("help-call", "Women & Child Helpline", "109");
+makeNewElement("govt-call", "Anti-Corruption Helpline", "106");
+makeNewElement("electricity-call", "Electricity Helpline", "16216");
+makeNewElement("brac-call", "Brac Helpline", "16445");
+makeNewElement("railway-call", "Bangladesh Railway Helpline", "163");
+
 // Call Buttons Function
 const callBtns = document.getElementsByClassName("call-btns");
 const callTitles = document.querySelectorAll(".card-content .text-xl");
 
-for (const btn of callBtns) {
-  btn.addEventListener("click", function (e) {
-    // for (const callTitle of callTitles) {
-    //   alert(callTitle.innerText);
-    //   return;
-    // }
-  });
-}
 callFunction("national-call", "📞 Calling National Emergency Number 999...");
 callFunction("police-call", "📞 Calling Police Helpline Number 999...");
 callFunction("fire-call", "📞 Calling Fire Service Number 999...");
